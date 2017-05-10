82337c9624500ed88ed74eafce1374569ffd9191
William Zamora <wzamora@mail.csuchico.edu>
2017-05-10 13:19:20 -0700

    Finished project 3

--- league.js
+++ league.js
@@ -1,21 +1,25 @@
 function playerRatings(){
-  var cookie1 = getCookie("t1PRCookie");
-  var cookie2 = getCookie("t2PRCookie");
-  var cookie3 = getCookie("t3PRCookie");
-  var cookie4 = getCookie("t4PRCookie");
-  if(cookie1 > cookie2 && cookie1 > cookie3 && cookie1 > 4){
+  if(getCookie("t1PRCookie") > getCookie("t2PRCookie") &&
+    getCookie("t1PRCookie") > getCookie("t3PRCookie")
+   && getCookie("t1PRCookie") > getCookie("t4PRCookie")){
     document.getElementById("playerR").innerHTML
     = "You thought the team with the best players was Juventus.";
   }
- if(cookie2 > cookie1 && cookie2 > cookie3 && cookie2 > 4){
+ else if(getCookie("t2PRCookie") > getCookie("t1PRCookie")
+ && getCookie("t2PRCookie") > getCookie("t3PRCookie")
+ && getCookie("t2PRCookie") > getCookie("t4PRCookie")){
     document.getElementById("playerR").innerHTML
     = "You thought the team with the best players was Manchester United.";
   }
-  if(cookie3 > cookie2 && cookie3 > cookie1 && cookie3 > 4){
+  else if(getCookie("t3PRCookie") > getCookie("t2PRCookie")
+  && getCookie("t3PRCookie") > getCookie("t1PRCookie")
+  && getCookie("t3PRCookie") > getCookie("t4PRCookie")){
     document.getElementById("playerR").innerHTML
     = "You thought the team with the best players was Barcelona.";
   }
-  if(cookie4 > cookie2 && cookie4 > cookie3 && cookie4 > 1){
+  else if(getCookie("t4PRCookie") > getCookie("t1PRCookie")
+  && getCookie("t4PRCookie") > getCookie("t2PRCookie")
+  && getCookie("t4PRCookie") > getCookie("t3PRCookie")){
     document.getElementById("playerR").innerHTML
     = "You thought the team with the best players was Bayern Munich.";
   }
@@ -24,62 +28,60 @@ function playerRatings(){
   }
 }
 function teamRatings(){
-  function playerRatings(){
-    var rcookie1 = getCookie("t1Rating");
-    var rcookie2 = getCookie("t2Rating");
-    var rcookie3 = getCookie("t3Rating");
-    var rcookie4 = getCookie("t4Rating");
-    if(rcookie1 > rcookie2 && rcookie1 > rcookie3 && rcookie1 > 4){
-      document.getElementById("teamR").innerHTML
-      = "You thought the best team was Juventus.";
+    if(getCookie("t1Rating") > getCookie("t4Rating")
+    && getCookie("t1Rating") > getCookie("t2Rating")
+    && getCookie("t1Rating") > getCookie("t3Rating")){
+      document.getElementById("teamR").innerHTML  = "You thought the best team was Juventus.";
     }
-   if(rcookie2 > rcookie1 && rcookie2 > rcookie3 && rcookie2 > 4){
-      document.getElementById("teamR").innerHTML
-      = "You thought the best team was Manchester United.";
+    else if(getCookie("t2Rating") > getCookie("t1Rating")
+    && getCookie("t2Rating") > getCookie("t4Rating")
+    && getCookie("t2Rating") > getCookie("t3Rating")){
+      document.getElementById("teamR").innerHTML  = "You thought the best team was Manchester United.";
     }
-    if(rcookie3 > rcookie2 && rcookie3 > rcookie1 && rcookie3 > 4){
-      document.getElementById("teamR").innerHTML
-      = "You thought the best team was Barcelona.";
+    else if(getCookie("t3Rating") > getCookie("t1Rating")
+    && getCookie("t3Rating") > getCookie("t2Rating")
+    && getCookie("t3Rating") > getCookie("t4Rating")){
+      document.getElementById("teamR").innerHTML = "You thought the best team was Barcelona.";
     }
-    if(rcookie4 > rcookie2 && rcookie4 > rcookie3 && rcookie4 > 1){
-      document.getElementById("teamR").innerHTML
-      = "You thought the best team was Bayern Munich.";
+    else if(getCookie("t4Rating") > getCookie("t3Rating")
+    && getCookie("t4Rating") > getCookie("t2Rating")
+    && getCookie("t4Rating") > getCookie("t1Rating")){
+      document.getElementById("teamR").innerHTML = "You thought the best team was Bayern Munich.";
     }
     else{
       document.getElementById("teamR").innerHTML= "Problem please try again.";
     }
-  }
 }
 function team1PR(){
-  var team1PR = document.getElementById("team1Players").value;
-  setCookie("t1PRCookie", team1PR,2);
+  var team1P = document.getElementById("team1Players").value;
+  setCookie("t1PRCookie",team1P,2);
 }
 function team2PR(){
-  var team2PR = document.getElementById("team2Players").value;
-  setCookie("t2PRCookie", team2PR,2);
+  var team2P = document.getElementById("team2Players").value;
+  setCookie("t2PRCookie", team2P,2);
 }
 function team3PR(){
-  var team3PR = document.getElementById("team3Players").value;
-  setCookie("t3PRCookie", team3PR,2);
+  var team3P = document.getElementById("team3Players").value;
+  setCookie("t3PRCookie", team3P,2);
 }
 function team4PR(){
-  var team4PR = document.getElementById("team4Players").value;
-  setCookie("t4PRCookie", team4PR,2);
+  var team4P = document.getElementById("team4Players").value;
+  setCookie("t4PRCookie", team4P,2);
 }
 function t1Rating(){
-  var team1Rating = document.getElementById("team1Rating").value;
+  var team1Rating = document.getElementById("team1Rating");
   setCookie("t1Rating", team1Rating,2);
 }
 function t2Rating(){
-  var team2Rating = document.getElementById("team2Rating").value;
+  var team2Rating = document.getElementById("team2Rating");
   setCookie("t2Rating", team2Rating,2);
 }
 function t3Rating(){
-  var team3Rating = document.getElementById("team3Rating").value;
+  var team3Rating = document.getElementById("team3Rating");
   setCookie("t3Rating", team3Rating,2);
 }
 function t4Rating(){
-  var team4Rating = document.getElementById("team4Rating").value;
+  var team4Rating = document.getElementById("team4Rating");
   setCookie("t4Rating", team4Rating,2);
 }
 function team1Quiz(){
@@ -88,7 +90,7 @@ function team1Quiz(){
     alert("You are correct");
   }
   else {
-    alert("Try again")
+    alert("Try again");
   }
 }
 function team2Quiz(){
@@ -97,7 +99,7 @@ function team2Quiz(){
     alert("You are correct");
   }
   else {
-    alert("Try again")
+    alert("Try again");
   }
 }
 function team3Quiz(){
@@ -106,7 +108,7 @@ function team3Quiz(){
     alert("You are correct");
   }
   else {
-    alert("Try again")
+    alert("Try again");
   }
 }
 function team4Quiz(){
@@ -115,7 +117,7 @@ function team4Quiz(){
     alert("You are correct");
   }
   else {
-    alert("Try again")
+    alert("Try again");
   }
 }
 //courtesy of w3schools, from: http://www.w3schools.com/js/js_cookies.asp
@@ -139,4 +141,34 @@ function getCookie(cname) {
     }
   }
   return "";
+}
+
+function showFavTeam(){
+  var myFavteam = getFavTeam();
+  document.getElementById("fav").innerHTML = myFavteam;
+}
+  function getFavTeam(){
+    var myFavoriteTeams= [ "Manchester United", "CF America", "Real Madrid"];
+    var randFavTeam = Math.floor(Math.random() * myFavoriteTeams.length)
+   return myFavoriteTeams[randFavTeam];
+}
+var teamPhotos = [
+  {picture:"http://www.juventus.com/media/images/posati/stagione-16-17/prima-squadra/Foto-Squadra_Ufficiale241016.jpg",from:"http://www.juventus.com/en/"},
+  {picture:"http://images.footyroom.com/avatars/65923", from:"http://footyroom.com/matches/79926210/juventus-vs-genoa/review"},
+  {picture:"http://s.weltsport.net/bilder/teamfotos/640x360/222.jpg", from:"http://www.worldfootball.net/teams/bayern-muenchen/"},
+  {picture:"http://images.footyroom.com/avatars/61840", from:"http://footyroom.com/matches/79923639/darmstadt-vs-bayern-munich/review"},
+  {picture:"http://e1.365dm.com/17/03/16-9/20/skysports-premier-league-manchester-united-usa-fans-supporters_3913406.jpg?20170320135405", from:"http://www.skysports.com/football/news/11667/10808866/manchester-united-confirm-pre-season-tour-of-the-usa"},
+  {picture:"http://images.thepeoplesperson.com/wp-content/uploads/2016/03/30150946/Manchester-United-min.jpg", from:"http://www.playbuzz.com/pixeln10/the-ultimate-manchester-united-quiz"},
+  {picture:"https://barbarashdwallpapers.com/wp-content/uploads/2013/11/fc-barcelona-wallpaper.jpg", from:"https://barbarashdwallpapers.com/fc-barcelona/"}
+];
+var current;
+function shuffleGallery(){
+  var randI;
+  do {
+    randI = Math.floor(Math.random() * teamPhotos.length);
+    document.getElementById("picture").src = teamPhotos[randI].picture;
+    document.getElementById("source").innerHTML = "Image courtesy of:" + teamPhotos[randI].from;
+  } while (randI == current);
+  current = randI;
+  console.log(teamPhotos[current]);
 }
